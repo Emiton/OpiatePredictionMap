60f9c4f1ee404f839013e62dfb704fa20cea48bd
matthewsilva <32271475+matthewsilva@users.noreply.github.com>
2018-10-13 02:40:01 -0400

    Add files via upload

--- opioidmap.py
+++ opioidmap.py
@@ -22,9 +22,16 @@ import time
 trainingDataFile = open(sys.argv[1])
 trainingCsvReader = csv.reader(trainingDataFile)
 
+dataRep = []
+for line in trainingCsvReader:
+    dataRep.append(line)
+
+
 data = []
 
-for line in trainingCsvReader:
+for i in range(1, len(dataRep)-2, 2):
+    line = dataRep[i]
+    line.append(dataRep[i+1])
     data.append(line)
     #data = data.astype(numpy.float)
     #classification = line[0]
@@ -32,5 +39,8 @@ for line in trainingCsvReader:
     #self.trainingData.append(trainDatum)
 
 print(data[0])
+print()
 print(data[1])
+print()
 print(data[2])
+print()
